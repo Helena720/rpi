ab4627c0523923d44f62938142ffda040284c371
Helena720 <helena.pejovic03@gmail.com>
2026-06-05 08:59:19 +0200

    Novi z1 v12
    V12 novi

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <wiringPi.h>
-#include <wiringPiI2C.h>
+#include <QFile>
+#include <QTextStream>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -9,21 +10,13 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Samo prikaz - korisnik ne moze da pomjera
-    ui->dial->setDisabled(true);
-    ui->verticalSlider->setDisabled(true);
+    //postavi QLCDNumber na 5 cifara
+    ui->lcdNumber->setDigitCount(5);
 
-    // Opseg 0-255 (8-bitni ADC)
-    ui->dial->setRange(0, 255);
-    ui->verticalSlider->setRange(0, 255);
-
-    // I2C inicijalizacija
-    i2c_fd = wiringPiI2CSetup(I2C_ADRESA);
-
-    // Tajmer - ocitava senzore jednom u sekundi
     tajmer = new QTimer(this);
-    connect(tajmer, SIGNAL(timeout()), this, SLOT(ocitajSenzore()));
-    tajmer->start(1000);
+    connect(tajmer, SIGNAL(timeout()),
+            this, SLOT(ocitajTemperaturu()));
+    tajmer->start(1000); //svake sekunde
 }
 
 MainWindow::~MainWindow()
@@ -31,25 +24,46 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::ocitajSenzore()
-{
-    int vrednosti[3];
-    // Kanal 0 - osvetljaj
-    // Kanal 1 - temperatura (NTC)
-    // Kanal 2 - trimer potenciometar
+void MainWindow::ocitajTemperaturu(){
+    //DS18B20 cuva podatke ovdje
+    QString putanja = "/sys/bus/w1/devices/";
+    QDir dir(putanja);
 
-    for (int i = 0; i < 3; i++) {
-        wiringPiI2CWrite(i2c_fd, 0x40 | i); // kontrolni bajt + kanal
-        wiringPiI2CRead(i2c_fd);             // odbaci prvo citanje (stara vrijednost)
-        vrednosti[i] = wiringPiI2CRead(i2c_fd); // stvarna vrijednost
+    //nadji folder senzora, on pocinje sa 28-
+    QStringList liste = dir.entryList(
+        QStringList() << "28-*", QDir::Dirs);
+    if(liste.isEmpty()){
+        ui->lcdNumber->display("Err");
+        return;
     }
 
-    // Osvetljaj -> vertikalni slider (invertovano: vise svjetla = vise na slideru)
-    ui->verticalSlider->setValue(255 - vrednosti[0]);
+    //otvori fajl sa temperaturom
+    QString fajl = putanja + liste.first() + "/w1_slave";
+    QFile f(fajl);
 
-    // Temperatura -> QLCDNumber (skaliranje 0-255 na 0-100)
-    ui->lcdNumber->display(vrednosti[1] / 255.0 * 100);
+    if(!f.open(QIODevice::ReadOnly)){
+        ui->lcdNumber->display("Err");
+        return;
+    }
+    QTextStream tok(&f);
+    QString sadrzaj = tok.readAll();
+    f.close();
 
-    // Trimer potenciometar -> dial
-    ui->dial->setValue(vrednosti[2]);
+    //provjera da li je ocitavanje uspjelo
+    if(!sadrzaj.contains("YES")){
+        ui->lcdNumber->display("Err");
+        return;
+    }
+
+    //izvuci temperaturu (t=XXXXX)
+    int index = sadrzaj.indexOf("t=");
+    if(index == -1){
+        ui->lcdNumber->display("Err");
+        return;
+    }
+
+    QString tempStr = sadrzaj.mid(index + 2).trimmed();
+    double temp = tempStr.toDouble() / 1000.0; //pretvori u C
+
+    ui->lcdNumber->display(temp);
 }
